f7824ad2162ff7f8e64537a70a14a38ca33c227d
OCADAH <alishbahammad@ocadu.ca>
2025-04-22 05:07:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-0; // this is a comment, which means javascript will not try to execute this part
-134151855;
-12341.1515;
-Infinity;
--13434;
--Infinity;
--0;
+var kitty = {
+    cute: true,
+    hungrys: 0.8,
+    lives: 9,
+    fur: {
+        fluffy: true,
+        colour: "red",
+    },
+    threats: null,
+    meow: function (){ // a fucntion contained as a key value pair of an object is called a method
+        return "meow";
+    }, // annonymous functions have no name
+};
 
-var kitty; // delcared a variable for store space in memory;
+console.log(kitty.meow()); // ()function invokation operator
 
-kitty = 5; // definition with a data type of number with the value 5
+console.log(kitty.two);
+// variable name that is not declared gives an error
+// a undeclared parameter of an object gives undefined and no error
 
-var Kitty = 24; // semi are end of statement, this line has a definition and declaration
-
+kitty.lives++;
 console.log(kitty);
 
-kitty = Kitty; // = assignment operator right to left
-
-console.log(kitty); 
-
-var doggy; // declared a variable but not defined a value
-console.log(doggy); // undefined
-
-// numbers, undefined, strings
-
-var prose = 'I LOVE "KITTIES" and Potatoes'; // ' is a string delimiter
-var moreprose = "But I don't have a 'kitten' and can't eat potatoes everyday";
-
-// booleans are either true or false
-
-var x = true;
-x = false;
-
-// objects
-var ball = {}; // empty object
-ball = null; // null is a datatype in javascript
-
-// operators
-5 + 5;
-kitty = 5 + 5;
-var kitty; // becomes undefined because we are now referring to a new place in memory
-
-kitty = 5 +"5"; // string concatenation operator
+kitty.two = 'two'; // object are changeable and mutable (you can add to them after they are created)
 console.log(kitty);
 
-console.log(kitty + "MAYOOO");
+// kitty.travel(); // error because you can't invoke undefined as a function
+
+console.log(typeof kitty.future++); // results in a type number with value NaN which is not a number
+
+console.log( kitty.lives / null );
+
+//null gets coerced to 0 when changing to type number!!
+
+// . accessor of an object
+kitty.fur.fluffy;
+
+if(kitty.cute){
+    console.log('true?');
+}else{
+    console.log('false?');
+}
+
+for(key in kitty){ // generics
+    console.log(key);
+    kitty[key]; // alternate accessor for objects in javascript
+    // kitty["meow" + count];
+}
+
+
+var x = {};
+x.y = 12; //mutates object with assignment
+x.z; // returns undefined
+x.z()// throws an error by trying to invoke undefined as a function
+x.z = function(){}; // method
+x.z(); // invoke the z method method and returns undefined unless modified
+
--- sketch.js
+++ sketch.js
@@ -1,16 +1,91 @@
+var redBrick = {
+    x: 0,
+    y: 0,
+    w: 30,
+    h: 30,
+    xSpeed: 3,
+    ySpeed: 3,
+    colour: 'red',
+    draw: function(){
+        fill (  this.colour  );
+        rect(this.x, this.y ,this.w, this.h);
+    },
+    move: function(){
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
 
-var x = 0;
-var y = 0;
+        if(this.x < 0 || this.x > width - this.w){
+            this.xSpeed *= -1;
+        }
+        if(this.y > height - this.h || this.y <0){
+            this.ySpeed *= -1;
+        }
+    }
+};
+
+var blueBrick = {
+    x: 0,
+    y: 0,
+    w: 30,
+    h: 30,
+    xSpeed: 2,
+    ySpeed: 5,
+    colour: 'blue',
+    draw: function(){
+        fill (  this.colour  );
+        rect(this.x, this.y ,this.w, this.h);
+    },
+    move: function(){
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if(this.x < 0 || this.x > width - this.w){
+            this.xSpeed *= -1;
+        }
+        if(this.y > height - this.h || this.y <0){
+            this.ySpeed *= -1;
+        }
+    }
+};
+
+var purpleBrick = {
+    x: 0,
+    y: 0,
+    w: 30,
+    h: 30,
+    xSpeed: 3,
+    ySpeed: 8,
+    colour: 'purple',
+    draw: function(){
+        fill (  this.colour  );
+        rect(this.x, this.y ,this.w, this.h);
+    },
+    move: function(){
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        if(this.x < 0 || this.x > width - this.w){
+            this.xSpeed *= -1;
+        }
+        if(this.y > height - this.h || this.y <0){
+            this.ySpeed *= -1;
+        }
+    }
+};
+
+// redBrick.x++ returns current value then increments
+// ++redBrick.x increments value and then returns
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(720,280);
 }
 
 function draw(){
-    background('#78cceb') // automatic semicolon insertion
-    rect(x,y,10,10);
-    x = x + 2;
-    x = x % 500; // modulo operator
-    y = y + 2;
-    y = y % 500;
-} 
+    background('grey');
+    redBrick.draw();
+    redBrick.move();
+    blueBrick.draw();
+    blueBrick.move();
+    purpleBrick.draw();
+    purpleBrick.move();
+};
